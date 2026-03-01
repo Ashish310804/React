e13b607f39f058c95a3a3808e995d1893bbcc127
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-03-01 21:32:51 +0530

    Worked On Props

--- Ash1-Props/src/App.jsx
+++ Ash1-Props/src/App.jsx
@@ -4,9 +4,9 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user='Aman Singh' age={18} img='' />
-      <Card user='Sarthak' age={28} img='' />
-      <Card user='Aditya' age={24} img='' />
+      <Card user='Ashish Dwivedi' age={21} img='https://plus.unsplash.com/premium_photo-1764699345499-50440f0bfb17?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='Harsh Chauhan' age={22} img='https://images.unsplash.com/photo-1770242555231-273794231d6f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D' />
+      <Card user='Aman Singh' age={20} img='https://images.unsplash.com/photo-1770701195265-8af0dc148446?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D' />
     </div>
   )
 }
--- Ash1-Props/src/components/Card.jsx
+++ Ash1-Props/src/components/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Card = () => {
-  return (
-    <div>
-        <div className='card'>
-        <h1>Ashish Dwivedi</h1>
-        <p> I am a student at Dev Bhoomi Utrakhand University.</p>
-      </div>
-    </div>
-  )
+const Card = (props) => {
+    return (
+        <div className="card">
+            <img src={props.img} alt="" />
+            <h1>{props.user}</h1>
+            <h1>{props.age}</h1>
+            <p>I am a student at Dev Bhoomi Utrakhand University.</p>
+            <button>View Profile</button>
+        </div>
+    )
 }
 
 export default Card
