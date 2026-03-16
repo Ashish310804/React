cc2174adf665d886531e760ebb009c0e399af3d4
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-03-16 20:08:47 +0530

    Update Card.jsx

--- 04-CardProject/src/Components/Card.jsx
+++ 04-CardProject/src/Components/Card.jsx
@@ -10,7 +10,7 @@ const Card = (props) => {
                 </div>
                 <div className="center">
                      <h3>{props.company} <span>{props.datePosted}</span></h3>
-                     <h2>{props.post}</h2>
+                    <h2>{props.post}</h2>
                     <div className='tag'>
                         <h4>{props.tag1}</h4>
                         <h4>{props.tag2}</h4>
